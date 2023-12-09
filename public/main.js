bf17f73c497fbf39c54cdeed0525084cbed1e536
Maria Sanli <marisanli.93@gmail.com>
2023-12-08 20:26:00 -0500

    added navbar stuff and edited startApp

--- public/main.js
+++ public/main.js
@@ -4,15 +4,6 @@ import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>Welcome to Vocab-YOU-lary!</h1>
-    <button class="btn btn-danger" id="click-me">Login</button><br />
-  `;
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
   // USE WITH FIREBASE AUTH
   ViewDirectorBasedOnUserAuthStatus();
 };
